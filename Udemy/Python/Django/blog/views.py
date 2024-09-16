e04e7c5339a703f4349327e663604746d1c981f7
anonimo <davidmaf@gmail.com>
2024-09-16 10:03:36 -0300

    Atualizando o curso Udemy Python, etapa do Django. Entendendo sobre Herança, Particionamento e Arquivos Estáticos.

--- Udemy/Python/Django/blog/views.py
+++ Udemy/Python/Django/blog/views.py
@@ -1,12 +1,29 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 def blog(request):
-    print(request)
-    print('Este print retorna no terminal')
-    return HttpResponse('Mensage Response for Blog')
+    print('Prompt This')
+
+    variavel_qualquer = {
+        'text': 'Enviado da view blog',
+        'title': 'Bloguinho - '
+    }
+
+    return render(
+        request,
+        'blog/index.html',
+        variavel_qualquer
+    )
 
 def exemplo(request):
     print(request)
-    print('Este print retorna no terminal')
-    return HttpResponse('Mensage Response for Blog Exemplo')
+
+    variavel_qualquer = {
+        'text': 'Enviado da view blog/exemplo',
+        'title': 'Exemplo - '
+    }
+
+    return render(
+        request,
+        'blog/exemplo.html',
+        variavel_qualquer
+    )
--- Udemy/Python/Django/home/views.py
+++ Udemy/Python/Django/home/views.py
@@ -1,6 +1,15 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 def home(request):
     print('Prompt This')
-    return HttpResponse('Mensage Response for page home')
+
+    variavel_qualquer = {
+            'text': 'Estamos na HOME',
+            'title': 'Home -'
+        }
+
+    return render(
+        request,
+        'home/index.html',
+        variavel_qualquer
+    )
