5678dc0161d71bf7c87e053be16a89758b1a9f2a
TioZiio <davidmaf@gmail.com>
2024-12-04 09:25:05 -0300

    Aula TDD com request

--- Python/Lab-Tests/Pessoas.py
+++ Python/Lab-Tests/Pessoas.py
@@ -1,6 +1,18 @@
+import requests
+
 class Pessoa:
     def __init__(self, name, lastname):
         self.name = name
         self.lastname = lastname
         self.data = False
-    
+        
+    def get_data(self):
+        resposta = requests.get('https://jsonplaceholder.typicode.com/users/1')
+        
+        if resposta.ok:
+            self.data = True
+            return 'CONECTADO'
+        else:
+            self.data = False
+            return 'ERRO 404'
+
--- Python/Lab-Tests/test_pessoa.py
+++ Python/Lab-Tests/test_pessoa.py
@@ -22,7 +22,18 @@ class TestPessoa(unittest.TestCase):
         self.assertFalse(self.p1.data)
 
     def test_pessoa_capturou_os_dados_OK(self):
-        pass
+        with patch('requests.get') as fake_request:
+            fake_request.return_value.ok = True
+
+            self.assertEqual(self.p1.get_data(), 'CONECTADO')
+            self.assertTrue(self.p1.data)
+            
+    def test_pessoa_capturou_os_dados_falha_404(self):
+        with patch('requests.get') as fake_request:
+            fake_request.return_value.ok = False
+
+            self.assertEqual(self.p1.get_data(), 'ERRO 404')
+            self.assertFalse(self.p1.data)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
